90fec0039a15ec016a7402c77876291330952da0
tejraj.nimbalkar <tejraj.nimbalkar@shamakcomputing.com>
2016-07-01 16:47:29 +0530

    Update

--- src/main/test/com/tejinfo/test/UserRestServiceTest.java
+++ src/main/test/com/tejinfo/test/UserRestServiceTest.java
@@ -1,12 +1,12 @@
-package com.humano.care.test;
+package com.tejinfo.test;
 
 import java.io.File;
 
 import org.junit.Test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.humano.care.model.User;
-import com.humano.care.model.UserList;
+import com.tejinfo.model.User;
+import com.tejinfo.model.UserList;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.WebResource;
 
@@ -15,12 +15,12 @@ public class UserRestServiceTest {
 	 @Test	
 	  public void insertUser() throws Exception {	
 		ObjectMapper objectMapper=new ObjectMapper();
-		User user=objectMapper.readValue(new File("//HumanoCareSys//src///testdata//userJson.json"), User.class);
+		User user=objectMapper.readValue(new File("//src///testdata//userJson.json"), User.class);
 		//we can assert our test cases on request
 		assert user.getMobileNo().isEmpty():"User Mobile Number should not be empty";
 		
 		Client client=new Client();
-		WebResource resource=client.resource("http://localhost:8080/HumanoCareSys/restapi/user/insert/");
+		WebResource resource=client.resource("http://localhost:8080/XXXXXXX/restapi/user/insert/");
 		//we can assert our test case on response
 		
 	  }
@@ -28,7 +28,7 @@ public class UserRestServiceTest {
 	 @Test
 	 public void getUser(){
 		 Client client=new Client();
-		 WebResource resource=client.resource("http://localhost:8080/HumanoCareSys/restapi/user/getUserList/");
+		 WebResource resource=client.resource("http://localhost:8080/XXXXXXX/restapi/user/getUserList/");
 		 UserList listUser =  resource.get(UserList.class);
 		 
 		//we can assert our test case on response
